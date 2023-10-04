cebf0b797b588591e3e05223c788a0e3df907b8b
Todd Carlsson <toddguitarc@gmail.com>
2023-10-04 13:44:59 +1300

    add basic footer to page container

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./footer.module.scss";
 
 export function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className={classNames(styles.container)}>
+      <div className={classNames(styles.content)}>
+        <div>Version</div>
+        <div>Docs</div>
+        <div>API</div>
+        <div>Help</div>
+      </div>
+    </footer>
+  );
 }
