cb2e1c733b295f4bd25ec951864c4050e5354b89
Todd Carlsson <toddguitarc@gmail.com>
2023-10-04 15:22:21 +1300

    add desktop footer styles

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import classNames from "classnames";
 import styles from "./footer.module.scss";
+import Link from "next/link";
 
 export function Footer() {
   return (
     <footer className={classNames(styles.container)}>
       <div className={classNames(styles.content)}>
-        <div>Version</div>
-        <div>Docs</div>
-        <div>API</div>
-        <div>Help</div>
+        <div className={classNames(styles.footerText)}>Version: 14.5.1</div>
+        <div className={classNames(styles.footerLinks)}>
+          <Link className={classNames(styles.footerLink)} href="#">
+            Docs
+          </Link>
+          <Link className={classNames(styles.footerLink)} href="#">
+            API
+          </Link>
+          <Link className={classNames(styles.footerLink)} href="#">
+            Help
+          </Link>
+          <Link className={classNames(styles.footerLink)} href="#">
+            Community
+          </Link>
+        </div>
+        <img
+          className={classNames(styles.footerLogo)}
+          src="/icons/logo-small.svg"
+          alt="logo"
+        />
       </div>
     </footer>
   );
