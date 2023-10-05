809f991f63b827966f79b159637d73f3242d3abf
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2023-10-05 16:29:25 +1300

    Update to Footer + Footer Tests (#14)
    * add footer files
    
    * add index barrel file to footer folder
    
    * add basic footer to page container
    
    * add desktop footer styles
    
    * add mobile responsive styles for footer and version number in footer
    
    * minor fix to logo image in footer
    
    * add cypress test for footer
    
    * fix cypress test for version number
    
    * remove version number from cypress test
    
    * minor fixes to footer and related cypress test

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,29 +1,53 @@
 import React from "react";
+import { version } from "package.json";
 import classNames from "classnames";
 import styles from "./footer.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import uuid from "react-uuid";
 
+export type footerLinkProps = {
+  id: string;
+  name: string;
+  href: string;
+};
 export function Footer() {
+  const footerLinks: footerLinkProps[] = [
+    {
+      id: uuid(),
+      name: "Docs",
+      href: "#",
+    },
+    {
+      id: uuid(),
+      name: "API",
+      href: "#",
+    },
+    {
+      id: uuid(),
+      name: "Help",
+      href: "#",
+    },
+    {
+      id: uuid(),
+      name: "Community",
+      href: "#",
+    },
+  ];
   return (
     <footer className={classNames(styles.container)}>
       <div className={classNames(styles.content)}>
-        <div className={classNames(styles.footerText)}>
-          Version: {process.env.NEXT_PUBLIC_APP_VERSION}
-        </div>
+        <div className={classNames(styles.footerText)}>Version: {version}</div>
         <div className={classNames(styles.footerLinks)}>
-          <Link className={classNames(styles.footerLink)} href="#">
-            Docs
-          </Link>
-          <Link className={classNames(styles.footerLink)} href="#">
-            API
-          </Link>
-          <Link className={classNames(styles.footerLink)} href="#">
-            Help
-          </Link>
-          <Link className={classNames(styles.footerLink)} href="#">
-            Community
-          </Link>
+          {footerLinks.map((link) => (
+            <Link
+              key={link.id}
+              className={classNames(styles.footerLink)}
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          ))}
         </div>
         <Image
           width={23}
