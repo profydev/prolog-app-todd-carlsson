b2c8e82a0ddd9f6e1231aaf0011f9fcd71a54ec7
Todd Carlsson <toddguitarc@gmail.com>
2023-10-05 16:16:37 +1300

    minor fixes to footer and related cypress test

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -1,17 +1,49 @@
+import { version } from "package.json";
+import { footerLinkProps } from "@features/layout";
+import uuid from "react-uuid";
+
+const footerLinks: footerLinkProps[] = [
+  {
+    id: uuid(),
+    name: "Docs",
+    href: "#",
+  },
+  {
+    id: uuid(),
+    name: "API",
+    href: "#",
+  },
+  {
+    id: uuid(),
+    name: "Help",
+    href: "#",
+  },
+  {
+    id: uuid(),
+    name: "Community",
+    href: "#",
+  },
+];
+
 describe("Footer", () => {
   beforeEach(() => {
     //open dashboard page
     cy.visit(`http://localhost:3000/dashboard`);
   });
   it("all items are present", () => {
-    cy.get("footer").contains(/docs/i);
-    cy.get("footer").contains(/api/i);
-    cy.get("footer").contains(/help/i);
-    cy.get("footer").contains(/community/i);
+    //check that each link is working and in the correct order
+    cy.get("footer")
+      .find("a")
+      .each(($el, index) => {
+        cy.wrap($el)
+          .contains(footerLinks[index].name)
+          .should("have.attr", "href");
+      });
+    //check for logo image
     cy.get("footer img")
       .should("be.visible")
       .should("have.attr", "src")
       .should("include", "/icons/logo-small.svg");
-    cy.get("footer").contains(/version:/i);
+    cy.get("footer").contains(`Version: ${version}`);
   });
 });
