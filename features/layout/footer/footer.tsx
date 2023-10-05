1150a7a4c600ed95c84258418394abd97fec2ee0
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2023-10-05 13:28:53 +1300

    Add Footer (#13)
    * add footer files
    
    * add index barrel file to footer folder
    
    * add basic footer to page container
    
    * add desktop footer styles
    
    * add mobile responsive styles for footer and version number in footer
    
    * minor fix to logo image in footer
    
    * add cypress test for footer
    
    * fix cypress test for version number
    
    * remove version number from cypress test

---
+++ features/layout/footer/footer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./footer.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+
+export function Footer() {
+  return (
+    <footer className={classNames(styles.container)}>
+      <div className={classNames(styles.content)}>
+        <div className={classNames(styles.footerText)}>
+          Version: {process.env.NEXT_PUBLIC_APP_VERSION}
+        </div>
+        <div className={classNames(styles.footerLinks)}>
+          <Link className={classNames(styles.footerLink)} href="#">
+            Docs
+          </Link>
+          <Link className={classNames(styles.footerLink)} href="#">
+            API
+          </Link>
+          <Link className={classNames(styles.footerLink)} href="#">
+            Help
+          </Link>
+          <Link className={classNames(styles.footerLink)} href="#">
+            Community
+          </Link>
+        </div>
+        <Image
+          width={23}
+          height={33}
+          className={classNames(styles.footerLogo)}
+          src="/icons/logo-small.svg"
+          alt="logo"
+        />
+      </div>
+    </footer>
+  );
+}
