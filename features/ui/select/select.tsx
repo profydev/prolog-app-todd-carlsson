f9e216e27630e32389fe772ecbc30bee59a87fcc
Todd Carlsson <toddguitarc@gmail.com>
2023-10-16 15:35:41 +1300

    add basic files and exports for select

---
+++ features/ui/select/select.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./select.module.scss";
+
+export function Select() {
+  return <div className={classNames(styles.select)}>select</div>;
+}
