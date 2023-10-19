e205451476a472a3cac764121883bef0b13e31ed
Todd Carlsson <toddguitarc@gmail.com>
2023-10-19 14:19:17 +1300

    add styles and functionality for select

---
+++ features/ui/select/select.stories.tsx
@@ -0,0 +1,63 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Select } from "./select";
+
+const meta = {
+  component: Select,
+  title: "UI/Select",
+  argTypes: {
+    disabled: {
+      control: "boolean",
+    },
+  },
+} satisfies Meta<typeof Select>;
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const select: Story = {
+  render: () => (
+    <div style={{ width: 320 }}>
+      <Select
+        options={[
+          "Phoenix Baker",
+          "Olivia Rhye",
+          "Lana Steiner",
+          "Demi Wilkinson",
+          "Candice Wu",
+          "Natali Craig",
+          "Drew Cano",
+        ]}
+      />
+    </div>
+  ),
+  // args: {
+  //     options: [
+  //         "Phoenix Baker",
+  //         "Olivia Rhye",
+  //         "Lana Steiner",
+  //         "Demi Wilkinson",
+  //         "Candice Wu",
+  //         "Natali Craig",
+  //         "Drew Cano"
+  //     ]
+  // }
+};
+export const selectDisabled: Story = {
+  render: () => (
+    <div style={{ width: 320 }}>
+      <Select
+        options={[
+          "Phoenix Baker",
+          "Olivia Rhye",
+          "Lana Steiner",
+          "Demi Wilkinson",
+          "Candice Wu",
+          "Natali Craig",
+          "Drew Cano",
+        ]}
+        disabled
+      />
+    </div>
+  ),
+};
--- features/ui/select/select.tsx
+++ features/ui/select/select.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import uuid from "react-uuid";
 import classNames from "classnames";
 import styles from "./select.module.scss";
+import Image from "next/image";
 
-export function Select() {
-  return <div className={classNames(styles.select)}>select</div>;
+type SelectProps = {
+  options: string[];
+  emptyText?: string;
+  disabled?: boolean;
+};
+
+export function Select({
+  options,
+  emptyText = "Choose an Option",
+  disabled = false,
+}: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(emptyText);
+
+  return (
+    <div
+      onClick={() => !disabled && setIsOpen(!isOpen)}
+      className={classNames(styles.selectWrapper)}
+    >
+      <div className={classNames(styles.select, isOpen && styles.open)}>
+        <div
+          className={classNames(
+            styles.selectTrigger,
+            selectedOption === emptyText ? styles.empty : "",
+            isOpen ? styles.focus : "",
+            disabled ? styles.disabled : "",
+          )}
+        >
+          <span>{selectedOption}</span>
+          <Image
+            style={{ transform: !isOpen ? "rotate(180deg)" : "" }}
+            src="/icons/chevron.svg"
+            height={20}
+            width={20}
+            alt="arrow"
+          />
+        </div>
+        <div className={classNames(styles.customOptions)}>
+          {options.map((item: string) => (
+            <span
+              key={uuid()}
+              onClick={() => setSelectedOption(item)}
+              className={classNames(
+                styles.customOption,
+                selectedOption === item && styles.selected,
+              )}
+              data-value={item}
+            >
+              {item}
+              {selectedOption === item && (
+                <Image
+                  src="/icons/check-select.svg"
+                  height={20}
+                  width={20}
+                  alt="check"
+                />
+              )}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
