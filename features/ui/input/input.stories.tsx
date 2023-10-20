c799784f534ac8eaa71a38d1e326557a650192cb
Todd Carlsson <toddguitarc@gmail.com>
2023-10-21 10:12:29 +1300

    add label, hint, disabled and focus states

--- features/ui/input/input.stories.tsx
+++ features/ui/input/input.stories.tsx
@@ -11,3 +11,24 @@ export default meta;
 type Story = StoryObj<typeof Input>;
 
 export const input: Story = {};
+export const icon: Story = {
+  args: {
+    iconSrc: "/icons/mail.svg",
+  },
+};
+export const disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+export const label: Story = {
+  args: {
+    label: "Email",
+  },
+};
+export const hint: Story = {
+  args: {
+    label: "Email",
+    hint: "This is a hint text to help user.",
+  },
+};
--- features/ui/input/input.tsx
+++ features/ui/input/input.tsx
@@ -1,15 +1,45 @@
 import React, { HtmlHTMLAttributes } from "react";
 import styles from "./input.module.scss";
 import classNames from "classnames";
+import Image from "next/image";
 
-type InputProps = HtmlHTMLAttributes<HTMLInputElement>;
+type InputProps = HtmlHTMLAttributes<HTMLInputElement> & {
+  iconSrc?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  label?: string;
+  hint?: string;
+};
 
-export function Input({ ...props }: InputProps) {
+export function Input({
+  iconSrc,
+  placeholder = "Type here",
+  disabled = false,
+  label,
+  hint,
+  ...props
+}: InputProps) {
   return (
-    <input
-      {...props}
-      placeholder="Type here"
-      className={classNames(styles.input)}
-    />
+    <div className={classNames(styles.inputContainer)}>
+      {label && <p className={classNames(styles.label)}>{label}</p>}
+      {iconSrc && (
+        <Image
+          className={classNames(styles.icon)}
+          src={iconSrc}
+          height={20}
+          width={20}
+          alt="icon"
+        />
+      )}
+      <input
+        {...props}
+        type="text"
+        placeholder={placeholder}
+        disabled={disabled}
+        className={classNames(styles.input)}
+        style={{ paddingLeft: iconSrc ? 42 : 14 }}
+      />
+      {hint && <p className={classNames(styles.hint)}>{hint}</p>}
+    </div>
   );
 }
